93dea4e009b0f9322a446dfcea31e98523c5d2e2
Beato <121357043+urc4@users.noreply.github.com>
2023-07-14 20:16:24 -0300

    Fazer animacao dos tanques com fluxos

--- obterPlanta.m
+++ obterPlanta.m
@@ -4,10 +4,14 @@ function planta = obterPlanta()
 % de niveis de altura.
 
 planta.tanque_1.A = 10;
-planta.tanque_1.h_max = 10; % aqui como faria para considerar: bloco de saturacao
+planta.tanque_1.altura_inicial = 5; % onde coloca no simulink, talvez aqui estariamos complicando ...
+planta.tanque_1.altura = 10; % aqui como faria para considerar: bloco de saturacao
+planta.tanque_1.largura = 10; 
 
 planta.tanque_2.A = 10;
-planta.tanque_2.h_max = 10; % aqui como faria para considerar: bloco de saturacao
+planta.tanque_2.altura_inicial = 5; % onde coloca no simulink
+planta.tanque_2.altura = 10; % aqui como faria para considerar: bloco de saturacao
+planta.tanque_2.largura = 10; 
 
 planta.R1 = 5;
 planta.R2 = 5;
