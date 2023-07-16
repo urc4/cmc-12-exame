d672b413966c2c1c64bd9b56add87d6646d0fa2f
danielquintao <danielquintaodemoraes@gmail.com>
2023-07-16 02:08:18 -0300

    funcoes genericas e particulares para metodo Nelder-Mead

---
+++ obterControladorNelderMead.m
@@ -0,0 +1,20 @@
+function controlador  = obterControladorNelderMead(requisitos, planta)
+% controlador = obterControladorAnalitico(requisitos, planta)
+% A
+% struct requisitos eh:
+% requisitos.tr: requisito de tempo de subida de 0 a 100 por cento.
+% requisitos.Mp: requisito de sobressinal para cada tanque
+% A struct planta contem os parametros da planta e pode ser obtida atraves
+% de planta = obterPlanta().
+% A saida da funcao eh a struct controlador:
+% controlador.tanque_1.Ki: ganho do termo integrativo do controlador de altura PID.
+% controlador.tanque_1.Kp: ganho do termo proporcional do controlador de altura PID.
+% controlador.tanque_1.Kd: ganho do termo derivativo do controlador de altura PID.
+% controlador.tanque_2.Ki: ganho do termo integrativo do controlador de altura PID.
+% controlador.tanque_2.Kp: ganho do termo proporcional do controlador de altura PID.
+% controlador.tanque_2.Kd: ganho do termo derivativo do controlador de altura PID.
+
+controlador.tanque_1 = projetarControladorTanque1NelderMead(requisitos, planta);
+controlador.tanque_2 = projetarControladorTanque2NelderMead(requisitos, planta);
+
+end
