a25c7ee610237554f93772cc5138ac98f0deca2c
Giuseppe Vicente <106415095+giuseppevb@users.noreply.github.com>
2023-07-19 13:45:29 -0300

    com penalizacao de ganho negativo

--- projetarControladorNelderMead.m
+++ projetarControladorNelderMead.m
@@ -1,13 +1,24 @@
 function [controlador,Jval] = projetarControladorNelderMead(requisitos, planta)
 
-% chute inicial
+% Define a função de custo
+J = @(K) funcaoCustoTrMpGenerica(requisitos, planta, {'Ki','Kp','Kd'}, @obterGf, true, K);
+
+% Define a função auxiliar que penaliza ganhos negativos
+Jaux = @(K) J(K) + sum(max(-K, 0));
+
+% Chute inicial
 controlador0 = projetarControladorTunando(requisitos, planta);
-% otimizacao
+
+% Otimização
 K0 = [controlador0.Ki, controlador0.Kp, controlador0.Kd];
 opcoes = optimset('Display', 'iter');
-J = @(K) funcaoCustoTrMpGenerica(requisitos, planta, {'Ki','Kp','Kd'}, @obterGf, true, K);
-KOtimo = fminsearch(J, K0, opcoes);
-Jval = J(xOtimo);
+KOtimo = fminsearch(Jaux, K0, opcoes);
+
+% Transforma ganho negativo em 0
+KOtimo = max(KOtimo, 0);
+
+% Extrai os parâmetros ótimos
+Jval = J(KOtimo);
 controlador.Ki = KOtimo(1);
 controlador.Kp = KOtimo(2);
 controlador.Kd = KOtimo(3);
